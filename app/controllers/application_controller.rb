d16af69a4a01d907ce4515f29c5ccc6da7a494ea
Raghav Bhatnagar <raghavbhatnagar96@gmail.com>
2018-03-26 19:13:25 +0530

    creating UOD successful.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,38 +2,35 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :first_user, :except => [:createUOD, :create_UOD]
   before_action :authenticate_user!, :except => [:createUOD, :create_UOD]
-
-  def createUOD
-   @world = World.new()
-  end
-
+  layout "auth", :only => [:createUOD, :create_UOD]
 
   def create_UOD
-    @world = World.new(UOD_params)
-    respond_to do |format|
-      if @world.save
-        format.html { redirect_to :root, notice: 'World was successfully created.' }
-        format.json { render :show, status: :created, location: @world }
-      else
-        format.html { render :new }
-        format.json { render json: :root.errors, status: :unprocessable_entity }
+    unless World.exists?
+      @world = World.new()
+      @world1 = params[:world]
+      @world[:title] = @world1[:title]
+      respond_to do |format|
+        if @world.save
+          format.html { redirect_to :root, notice: 'World was successfully created.' }
+          format.json { render :show, status: :created, location: @world }
+        else
+          format.html { render :new }
+          format.json { render json: :root.errors, status: :unprocessable_entity }
+        end
       end
     end
   end
-
+ 
 
   private
   def first_user
   	unless World.exists?
   		redirect_to home_createUOD_url
   	end
-  end
-
-  
+  end  
 
   def UOD_params
-  	params.require(:world).permit(:title)
+    params.require(:world).permit(:title)
   end
-   
-
+ 
 end
